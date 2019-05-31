159dda1ebcd4204a09de1355840337a7f9492a15
Chris Watts <chris.watts@odb.org>
2019-05-31 10:03:48 -0400

    Through lesson 157 getting total number of ingredients and dealing with none added yet

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import styles from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientName => {
       return [...Array(props.ingredients[ingredientName])]
         .map((_,i)=> <BurgerIngredient type={ingredientName} key={ingredientName + i}/>)
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+  if(transformedIngredients.length === 0){
+    transformedIngredients=<p>Please start adding ingredients</p>
+  }
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type={'bread-top'}/>
